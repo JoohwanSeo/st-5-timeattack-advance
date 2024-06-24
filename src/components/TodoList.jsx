f0cb1cadc425d48cd782f6cc818fd9cf9035f5d6
Joohwan Seo <songqkaoswe@naver.com>
2024-06-24 16:35:19 +0900

    todolist 완료

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,4 +1,4 @@
-import { useQuery, useQueryClient } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { FaHeart, FaRegHeart } from "react-icons/fa";
 import { useNavigate } from "react-router-dom";
 import { todoApi } from "../api/todos";
@@ -20,24 +20,40 @@ export default function TodoList() {
 
   // TODO: 아래 handleLike 로 구현되어 있는 부분을 useMutation 으로 리팩터링 해보세요. 모든 기능은 동일하게 동작해야 합니다.
   const queryClient = useQueryClient();
-  const handleLike = async (id, currentLiked) => {
-    const previousTodos = [...todos];
-    try {
-      queryClient.setQueryData(["todos"], (prev) =>
-        prev.map((todo) =>
-          todo.id === id ? { ...todo, liked: !todo.liked } : todo,
-        ),
-      );
-      await todoApi.patch(`/todos/${id}`, {
+
+  const handleLikeClick = useMutation(
+    ({ id, currentLiked }) =>
+      todoApi.patch(`/todos/${id}`, {
         liked: !currentLiked,
-      });
-    } catch (err) {
-      console.error(err);
-      queryClient.setQueryData(["todos"], previousTodos);
-    } finally {
-      refetch();
+      }),
+
+    {
+      onMutate: async ({ id, currentLiked }) => {
+        await queryClient.cancelQueries("todos");
+        const previousTodos = queryClient.getQueryData("todos");
+
+        queryClient.setQueryData("todos", (pre) =>
+          pre.map((todo) =>
+            todo.id === id ? { ...todo, liked: !todo.liked } : todo
+          )
+        );
+        return {previousTodos}
+      },
+      onError: (err, newTodos, context) => {
+        console.error(err)
+        if (context?.previousTodos) {
+          queryClient.setQueryData('todos', context.previousTodos)
+        }
+      },
+      onSettled: () => {
+        refetch()
+      }
     }
-  };
+  );
+
+  const handleLike = (id, currentLiked) => {
+    handleLikeClick.mutate({id, currentLiked})
+  }
 
   if (isPending) {
     return <div style={{ fontSize: 36 }}>로딩중...</div>;
